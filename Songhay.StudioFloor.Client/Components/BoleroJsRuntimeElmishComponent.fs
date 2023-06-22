a0bdcdbd311b865af15f34eee56e73a827c5374a
Bryan Wilhite <bryan.wilhite@gmail.com>
2023-06-21 21:10:43 -0700

    #12 🔨🍱

--- Songhay.StudioFloor.Client/Components/BoleroJsRuntimeElmishComponent.fs
+++ Songhay.StudioFloor.Client/Components/BoleroJsRuntimeElmishComponent.fs
@@ -2,6 +2,7 @@ namespace Songhay.StudioFloor.Client.Components
 
 open Bolero
 open Bolero.Html
+open Elmish
 
 open Songhay.Modules.Bolero.Models
 open Songhay.Modules.Bolero.Visuals.Bulma.CssClass
@@ -12,6 +13,22 @@ open Songhay.StudioFloor.Client.Models
 
 type BoleroJsRuntimeElmishComponent() =
     inherit ElmishComponent<StudioFloorModel, StudioFloorMessage>()
+
+    let blockWrapperRef = HtmlRef()
+
+    let demoBlock (model: StudioFloorModel) (dispatch: Dispatch<StudioFloorMessage>) =
+        let styleList =
+            [
+                "--main-bg-color: brown"
+                "background-color: var(--main-bg-color)"
+            ]
+
+        div {
+            attr.style (styleList |> List.reduce (fun a i -> $"{a};{i}"))
+            attr.ref blockWrapperRef
+        }
+
+    static let click = DomElementEvent.Click
 
     static member EComp model dispatch =
         ecomp<BoleroJsRuntimeElmishComponent, _, _> model dispatch { attr.empty() }
@@ -32,4 +49,5 @@ type BoleroJsRuntimeElmishComponent() =
                     (subtitle DefaultBulmaFontSize) @ [ ColorPrimary.TextCssClass; m (T, L1) ] |> CssClasses.toHtmlClassFromList
                     text "This utility meets the concern of Blazor-JavaScript interoperability."
                 }
+                demoBlock model dispatch
             })
